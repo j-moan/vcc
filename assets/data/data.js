0e8088cf61026799e14580f20a42ddcaf024e25d
James Moan <jmmoan@gmail.com>
2026-07-26 23:18:18 +0000

    Publish classroom content 2026-07-26T23:18:17.986Z

--- assets/data/data.js
+++ assets/data/data.js
@@ -22,6 +22,12 @@ window.CLASSROOM_SITE = {
           "active": true
         },
         {
+          "id": "section-2873ecbd-8a3f-49c2-9dc1-93bac71480f5",
+          "type": "section",
+          "label": "seperation",
+          "active": true
+        },
+        {
           "id": "nav-math",
           "type": "navigation",
           "container": "math",
@@ -42,12 +48,6 @@ window.CLASSROOM_SITE = {
           "label": "picture test",
           "image": "reading.jpg",
           "target": "",
-          "active": true
-        },
-        {
-          "id": "section-2873ecbd-8a3f-49c2-9dc1-93bac71480f5",
-          "type": "section",
-          "label": "seperation",
           "active": true
         }
       ]
